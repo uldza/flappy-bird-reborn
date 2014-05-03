c7f6812ba8a646e62b5197ab40a6fbafff619473
Uldzh <uldis.megnis@gmail.com>
2014-05-03 12:53:44 +0300

    get rid of unnecessary express stuff

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 var express = require('express');
+
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 app.listen(3000);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
